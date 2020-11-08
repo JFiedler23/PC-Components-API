03e15cd91307451e997703f740bcb908f57bbed9
JFiedler23 <43707498+JFiedler23@users.noreply.github.com>
2020-11-08 15:52:42 -0600

    Update scrape-handler.js

--- source/scrape-handler.js
+++ source/scrape-handler.js
@@ -5,7 +5,7 @@ async function getAllStores(urls){
   var items = {};
   for(let i = 0; i < urls.length; i++){
     if(urls[i].store === "newegg"){
-      //items.newegg = await Newegg_Scrape.getItems(urls[i].url);
+      items.newegg = await Newegg_Scrape.getItems(urls[i].url);
     }
     else if(urls[i].store === "microcenter"){
       items.microcenter = await Micro_Center_Scrape.getItems(urls[i].url);
