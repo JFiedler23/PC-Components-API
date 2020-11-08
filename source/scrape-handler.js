b89f66e4bec806b14ea28f61ac70f8d81179b469
JFiedler23 <43707498+JFiedler23@users.noreply.github.com>
2020-11-08 15:52:52 -0600

    Update scrape-handler.js

--- source/scrape-handler.js
+++ source/scrape-handler.js
@@ -26,32 +26,5 @@ module.exports = {
         return items
         break;
     }
-
-    /*
-    //getting gpus from all stores
-    if(store === "all"){
-      for(let i = 0; i < urls.length; i++){
-        if(urls[i].store === "newegg"){
-          items.newegg = await Newegg_scrape.getItems(urls[i].url);
-        }
-        else if(urls[i].store === "amazon"){
-          items.amazon = await Amazon_Scrape.getItems(urls[i].url);
-        }
-      }
-      return items;
-    }
-    //user requested products from newegg only
-    else if(store === "newegg"){
-      for(let i = 0; i < urls.length; i++){
-        if(urls[i].store === "newegg"){
-          items.newegg = await Newegg_scrape.getItems(urls[i].url);
-        }
-      }
-      return items;
-    }
-    else{
-      console.log("Store not supported\n");
-    }
-    */
   }
 };
